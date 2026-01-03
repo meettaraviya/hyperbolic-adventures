5a3b0544e4d8441d1cd220c1a916c2db9e77282a
Meet Taraviya <meet11061997@gmail.com>
2026-01-02 22:20:03 -0800

    Refactor code structure: extract CLI to separate module and add GUI interface

--- hypertle.py
+++ hypertle.py
@@ -1,11 +1,11 @@
 import math
 
 from canvas import *
-
+from turtle import RawTurtle, Turtle
+from cli import parse
 import threading
 
-
-class Hypertle(Canvas):
+class Hypertle_BACKUP(Canvas, RawTurtle):
     def __init__(self, r: int, turtle_scale: int):
         Canvas.__init__(self, r)
         self.turtle_shape = [(1., 0.), (-1., 1.), (0., 0.), (-1., -1.)]
@@ -51,106 +51,39 @@ class Hypertle(Canvas):
         pygame.draw.polygon(self.surface, self.color, turtle_points)
 
 
-def get_float(q):
-    top = q.pop(0)
-    try:
-        return float(top)
-    except ValueError:
-        match top:
-            case 'e':
-                return math.e
-            case 'tau':
-                return math.tau
-            case 'pi':
-                return math.pi
-            case '+':
-                return get_float(q) + get_float(q)
-            case '*':
-                return get_float(q) * get_float(q)
-            case '/':
-                return get_float(q) / get_float(q)
-            case '-':
-                return get_float(q) - get_float(q)
-            case '√':
-                return math.sqrt(get_float(q))
-            case 's':
-                return math.sin(get_float(q))
-            case 'c':
-                return math.cos(get_float(q))
-            case 't':
-                return math.tan(get_float(q))
-            case 'as':
-                return math.asin(get_float(q))
-            case 'ac':
-                return math.acos(get_float(q))
-            case 'at':
-                return math.atan(get_float(q))
-            case 'ash':
-                return math.asinh(get_float(q))
-            case 'ach':
-                return math.acosh(get_float(q))
-            case 'ath':
-                return math.atanh(get_float(q))
-            case 'sh':
-                return math.sinh(get_float(q))
-            case 'ch':
-                return math.cosh(get_float(q))
-            case 'th':
-                return math.tanh(get_float(q))
+class Hypertle(Turtle, Hypertle_BACKUP):
+    def __init__(self, R, turtle_scale):
+        Turtle.__init__(self)
+        Hypertle_BACKUP.__init__(self, R, turtle_scale)
+        self.turtle = Turtle()
 
+    def draw(self):
+        Canvas.draw(self)
+        self.__draw_turtle()
+    
+    def fd(self, x):
+        Hypertle_BACKUP.fd(self, x)
+        self.turtle.fd(x)
+    
+    def bk(self, x):
+        Hypertle_BACKUP.bk(self, x)
+        self.turtle.bk(x)
+    
+    def rt(self, theta):
+        Hypertle_BACKUP.rt(self, theta)
+        self.turtle.rt(theta)
 
-def parse(cmd_queue):
-    global exit_flag
-    while len(cmd_queue) > 0:
-        token = cmd_queue.pop(0)
-        match token:
-            case 'q':
-                exit_flag = True
-            case 'circle':
-                r = get_float(cmd_queue)
-                turtle.circle(r)
-            case 'poly':
-                n = int(cmd_queue.pop(0))
-                theta = get_float(cmd_queue)
-                turtle.polygon(n, theta)
-            case 'pd':
-                turtle.pd()
-            case 'pu':
-                turtle.pu()
-            case 'fd':
-                x = get_float(cmd_queue)
-                turtle.fd(x)
-            case 'bk':
-                x = get_float(cmd_queue)
-                turtle.bk(x)
-            case 'lt':
-                theta = get_float(cmd_queue)
-                turtle.lt(theta)
-            case 'rt':
-                theta = get_float(cmd_queue)
-                turtle.rt(theta)
-            case 'cl':
-                turtle.clear()
-            case 'repeat':
-                n = int(cmd_queue.pop(0))
-                if cmd_queue.pop(0) == '[':
-                    depth = 1
-                    i = 0
-                    for cmd in cmd_queue:
-                        if cmd == '[':
-                            depth += 1
-                        elif cmd == ']':
-                            depth -= 1
-                            if depth == 0:
-                                break
-                        i += 1
+    def lt(self, theta):
+        Hypertle_BACKUP.lt(self, theta)
+        self.turtle.lt(theta)
 
-                    sub_cmd = cmd_queue[:i]
-
-                    for j in range(n):
-                        parse(sub_cmd.copy())
-
-                    cmd_queue = cmd_queue[i + 1:]
+    def __draw_turtle(self):
+        turtle_points = [
+            pygame.math.Vector2(p).rotate(self.turtle_phase) * self.turtle_scale +
+            pygame.math.Vector2(self.R, self.R)
+            for p in self.turtle_shape
+        ]
+        pygame.draw.polygon(self.surface, self.color, turtle_points)
 
 
 
@@ -183,4 +116,5 @@ if __name__ == '__main__':
 
     pygame.display.quit()
     cmd_thread.join()
+
 
